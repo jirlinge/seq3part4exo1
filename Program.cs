40e6261a0e10b6ebec1c472390170a973d14ad98
jirlinge <julienirlinger@gmail.com>
2021-02-18 14:23:57 +0100

    version corrigé

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] myArray = new string[] { "lundi", "mardi", "mercredi","jedi","vendredi","samedi" };
+
+
+
+            Console.WriteLine(myArray[1] +"\n"+ myArray[4]);
+            Console.WriteLine(myArray[0] + "\n" + myArray[3]);
+
+            myArray [3] ="jeudi";
+
+            Console.WriteLine(myArray[3]);
+            
+
+            Array.Resize<string>(ref myArray, 7); 
+            myArray[6] = "Dimanche";
+            Console.WriteLine(myArray[6]);
+
+
+
+
+        }
+    }
+    }
+
